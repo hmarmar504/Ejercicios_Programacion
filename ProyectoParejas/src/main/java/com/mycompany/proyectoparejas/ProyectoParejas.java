ceeab2322cd35e36144a8f090198bae84efe5886
Hector Martinez <hmarmar504@g.educaand.es>
2024-11-19 14:41:20 +0100

    menu

--- ProyectoParejas/src/main/java/com/mycompany/proyectoparejas/ProyectoParejas.java
+++ ProyectoParejas/src/main/java/com/mycompany/proyectoparejas/ProyectoParejas.java
@@ -1,6 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.proyectoparejas;
 import java.util.Arrays;
@@ -17,14 +15,64 @@ public class ProyectoParejas {
             if (i<10){
                 tabas[i] = (char) (i + 48);
             }
-            else if(i>10&&i<35){
+            else if(i>=10&&i<34){
                 tabas[i] = (char) (i+55);
             }
             else{
-                tabas[i] = (char) (i+87);
+                tabas[i] = (char) (i+63);
             }
         }
         System.out.println(Arrays.toString(tabas));
+    }
+    
+    //Menu principal de opciones
+    public static void menuOpciones(int[]ajustes){
+        
+        //Ajustes predeterminados del juego
+        ajustes[0]=4;//Ancho de la tabla Maximo 10
+        ajustes[1]=4;//Alto de la tabla Maximo 10
+        ajustes[2]=1;//Zoom Maximo 3
+        ajustes[3]=0;//Dificultad de la maquina: 0=No maquina, 1=Facil, 2=Medio, 3=Dificil
+        ajustes[4]=3;//Cantidad de errores permitidos
+        ajustes[5]=3;//Tiempo de muestra mS
+        ajustes[6]=0;//Variable de salida del juego: 0=Seguir jugando, otro=Salir del juego
+        
+        int selec;
+        boolean tabNoPar=false,salida=false;
+        Scanner sc = new Scanner (System.in);
+        do{
+        //Seleccion de los ajustes del juego
+        System.out.println("Elija sus opciones de juego");
+        System.out.println("1.- Tamaño del tablero");
+        System.out.println("2.- Dificultad");
+        System.out.println("3.- Zoom");
+        System.out.println("4.- Cantidad de errores");
+        System.out.println("5.- Tiempo de muestra");
+        System.out.println("6.- Salir de los ajustes");
+        System.out.println("7.- Salir del juego");
+        
+        selec=sc.nextInt();
+        switch(selec){
+            case 1 -> {//Indicacion del tamaño del tablero
+                    System.out.println("Opciones de tamaño");
+                    do{
+                    System.out.println("introduzca el tamaño horizontal");
+                    ajustes[0]=sc.nextInt();
+                    System.out.println("Introduzca el tamaño vertical");
+                    ajustes[1]=sc.nextInt();
+            
+                    if((ajustes[0]%ajustes[1]==0)||(ajustes[1]%ajustes[0]==0)){
+                        tabNoPar=true;
+                    }
+                    else{
+                        System.out.println("Los valores introducidos no son validos, introduzcalos otra vez");
+                    }
+                    }while(tabNoPar=false);
+            }
+            case 2 -> {//Seleccion de dificultad
+                System.out.println("b");}
+        }
+        }while(salida);
     }
 
     public static void main(String[] args) {
@@ -34,17 +82,22 @@ public class ProyectoParejas {
         char intentos [][];
         char muestra [][];
         //variables generales
-        char ascii;
         char asciiGuardado [];
+        int ajustes[] = new int[8];
         char asciiIntro[];
-        int dimx,dimy,fallos,cant;
-        
+        int dimx,dimy,fallos,totalCasi;
         Scanner sc = new Scanner (System.in);
-        cant=sc.nextInt();
-        asciiGuardado = new char[cant];
+        do{
+        menuOpciones(ajustes);
         
         
-        ascii(asciiGuardado,cant);
+        totalCasi=sc.nextInt();
+        asciiGuardado = new char[totalCasi];
+        
+        
+        ascii(asciiGuardado,totalCasi);
         System.out.println(Arrays.toString(asciiGuardado));
+        }while(ajustes[6]!=0);
     }
+        
 }
