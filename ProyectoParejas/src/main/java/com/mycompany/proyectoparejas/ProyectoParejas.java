078a16d42c30cf9efae4a071451373bbc3bd8bf7
Hector Martinez <hmarmar504@g.educaand.es>
2024-11-25 14:38:41 +0100

    cambios proyecto

--- ProyectoParejas/src/main/java/com/mycompany/proyectoparejas/ProyectoParejas.java
+++ ProyectoParejas/src/main/java/com/mycompany/proyectoparejas/ProyectoParejas.java
@@ -88,12 +88,13 @@ public class ProyectoParejas {
                 case 1 -> {//Indicacion del tamaño del tablero
                         System.out.println("Opciones de tamaño");
                         do{
-                        System.out.println("introduzca el tamaño horizontal");
+                        System.out.println("introduzca el tamaño horizontal (maximo 10)");
                         ajustes[0]=sc.nextInt();
-                        System.out.println("Introduzca el tamaño vertical");
+                        System.out.println("Introduzca el tamaño vertical (maximo 10)");
                         ajustes[1]=sc.nextInt();
-
-                        if((ajustes[0]%ajustes[1]==0)||(ajustes[1]%ajustes[0]==0)){
+                        
+                        //Confirmacion del tamaño del tablero
+                        if(((ajustes[0]*ajustes[1])%2==0)&&(ajustes[0]<11)&&(ajustes[1]<11)&&(ajustes[1]>1)&&(ajustes[0]>1)){
                             fallo=true;
                         }
                         else{
@@ -169,8 +170,14 @@ public class ProyectoParejas {
         
     }
     //Rellenado de tabla vacia
-    public static void tabVacia(char[][]){
+    public static void tabVacia(char vacio[][]){
         
+    
+        for (int a = 0; a < vacio.length; a++) {
+            for (int b = 0; b < vacio[0].length; b++) {
+                vacio[a][b]=' ';
+            }
+        }
     }
     //Imprimir tabla con la cuadricula hecha(mas o menos)
     public static void imprimirTablero(char[][]tablero){
@@ -187,6 +194,79 @@ public class ProyectoParejas {
             System.out.print("-");
             System.out.println();
         }
+    }
+    
+    public static void selec(char[][]vacio,char[][]lleno,char[][]visto){
+        boolean selec=false,victoria;
+        String casilla1,casilla2;
+        int cas1,cas11,cas2,cas22;
+        Scanner sc = new Scanner (System.in);
+        do{
+            do{//Eleccion de la primera casilla
+               // if //Filtro para entradas no validas
+                System.out.println("Selecciona una casilla");
+                casilla1=sc.next();
+                cas1=Character.getNumericValue(casilla1.charAt(0));
+                cas11=Character.getNumericValue(casilla1.charAt(1));
+                //Sustitucion de la casilla elegida
+                if(vacio[cas1][cas11]==' '){
+                    visto[cas1][cas11]=lleno[cas1][cas11];
+                    vacio[cas1][cas11]=lleno[cas1][cas11];
+                    selec=true;
+                    imprimirTablero(vacio);
+                }
+                
+                else{
+                    System.out.println("Esa casilla ya esta elegida");
+                }
+                
+            }while(selec==false);
+
+            selec=false;
+
+            do{//Eleccion de la segunda casilla
+
+                System.out.println("Selecciona la segunda casilla");
+                casilla2=sc.next();
+                
+                //Sustitucion de la casilla elegida
+                if(vacio[Character.getNumericValue(casilla2.charAt(0))][Character.getNumericValue(casilla2.charAt(1))]==' '){
+                    visto[Character.getNumericValue(casilla2.charAt(0))][Character.getNumericValue(casilla2.charAt(1))]=lleno[Character.getNumericValue(casilla2.charAt(0))][Character.getNumericValue(casilla2.charAt(1))];
+                    vacio[Character.getNumericValue(casilla2.charAt(0))][Character.getNumericValue(casilla2.charAt(1))]=lleno[Character.getNumericValue(casilla2.charAt(0))][Character.getNumericValue(casilla2.charAt(1))];
+                    selec=true;
+                    imprimirTablero(vacio);
+                }
+
+                else{
+                    System.out.println("Esa casilla ya esta elegida");
+                }
+
+            }while(selec==false);
+
+            selec=false;
+
+            if(vacio[Character.getNumericValue(casilla2.charAt(0))][Character.getNumericValue(casilla2.charAt(1))]==vacio[Character.getNumericValue(casilla1.charAt(0))][Character.getNumericValue(casilla1.charAt(1))]){
+
+                System.out.println("Pareja encontrada");
+            }
+            else{
+                System.out.println("Prueba otra vez");
+                vacio[Character.getNumericValue(casilla2.charAt(0))][Character.getNumericValue(casilla2.charAt(1))]=' ';
+                vacio[Character.getNumericValue(casilla1.charAt(0))][Character.getNumericValue(casilla1.charAt(1))]=' ';
+                imprimirTablero(vacio);
+            }
+            
+            //Comprovacion de tablero completado
+            victoria=true;
+            for (int a = 0; a < vacio.length; a++) {
+                for (int b = 0; b < vacio[0].length; b++) {
+                    if(vacio[a][b]==' '){
+                        victoria=false;
+                    }
+                }
+            }
+        }while(victoria==false);
+        System.out.println("Partida terminado");
     }
     public static void main(String[] args) {
         //declaracion de varialbes
@@ -219,13 +299,21 @@ public class ProyectoParejas {
                 asciiGuardado=new char [ajustes[0]*ajustes[1]];
                 ascii(asciiGuardado);
                 
-                //Inicializacion de la tabla
+                //Inicializacion y rellenado de la tabla general
                 general = new char [ajustes[0]][ajustes[1]];
-
-                //Rellenado de la tabla
                 genTabla(ajustes,general,asciiGuardado);
                 
+                //Inicializacion y rellenado de las tablas secundarias
+                mostrados = new char [ajustes[0]][ajustes[1]];
+                salida = new char [ajustes[0]][ajustes[1]];
+                tabVacia(mostrados);
+                tabVacia(salida);
                 imprimirTablero(general);
+                imprimirTablero(mostrados);
+                
+                //Seleccion de casilla
+                selec(salida,general,mostrados);
+                
             }//Cierre de condicion de juego cerrado
         }while(ajustes[6]==0);
     }
